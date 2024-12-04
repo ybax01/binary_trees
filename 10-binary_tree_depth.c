4ee0c136dd653cb9f74840b7f8dfb940df369766
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-12-04 15:48:12 +0100

    Two

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,6 +11,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	size_t depth = 0;
 	if (tree->parent)
-		return (1 + binary_tree_depth(tree->parent));
+		depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
 }
